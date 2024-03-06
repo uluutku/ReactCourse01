bb6f64a88ad865f33017f5e3815574b2bdf700bc
Utku Ulu <utku.ulu@bilgeadamboost.com>
2024-03-06 20:57:49 +0300

    added refresh function

--- 08-MyCoursesApp/my-courses-app/src/App.jsx
+++ 08-MyCoursesApp/my-courses-app/src/App.jsx
@@ -1,42 +1,47 @@
-import { useEffect , useState } from 'react'
-import './App.css'
-import axios from "axios"
-import Courses from './Courses'
-import Loading from './Loading'
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import axios from "axios";
+import Courses from './Courses';
+import Loading from './Loading';
 
 function App() {
-
-  const [courses, setCourses] = useState([])
-  const [loading, setLoading] = useState(true)
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const deleteCourseById = (toDeleteId) => {
-    const afterDeletedCourses = courses.filter((mappedByFilterCourse) => mappedByFilterCourse.id !== toDeleteId)
+    const afterDeletedCourses = courses.filter((mappedByFilterCourse) => mappedByFilterCourse.id !== toDeleteId);
     setCourses(afterDeletedCourses);
-  }
+  };
 
   const fetchCourses = async () => {
-    setLoading(true)
-    try{
-    const apiResponse =  await axios.get("http://localhost:3000/courses");
-    setCourses(apiResponse.data);
-    setLoading(false)
+    setLoading(true);
+    try {
+      const apiResponse = await axios.get("http://localhost:3000/courses");
+      setCourses(apiResponse.data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
     }
-    catch(error){
-    setLoading(false)
-    }
-    
-  }
+  };
 
   useEffect(() => {
     fetchCourses();
-  },[])
+  }, []);
 
   return (
     <div className='app-component'>
-    {loading ? (<Loading/>) : 
-    <Courses courses={courses} relayDelete={deleteCourseById} />}
+      {loading ? <Loading /> : 
+        (courses.length === 0 ? 
+        
+        <div className='refresh-container'>
+            <h2>Hiçbir kurs yok.</h2>
+            <button className='refresh-btn' onClick={() => {fetchCourses()} }>Kursları yenile</button>
+        </div> 
+        
+        : <Courses courses={courses} relayDelete={deleteCourseById} />)
+      }
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
