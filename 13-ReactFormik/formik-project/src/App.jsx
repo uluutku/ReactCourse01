bcadf30ea80e2ab6bd4408f556dfe07224e925dd
Utku Ulu <utku.ulu@bilgeadamboost.com>
2024-03-20 18:47:23 +0300

    Formik lesson added

---
+++ 13-ReactFormik/formik-project/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import GeneralForm from "./components/GeneralForm";
+import PortalForm from "./components/PortalForm";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Routes>
+          <Route path="/" element={<GeneralForm />} />
+          <Route path="/portal" element={<PortalForm />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ 13-ReactFormik/formik-project/src/components/GeneralForm.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function GeneralForm() {
+  return (
+    <div>
+      <form>
+        <div>
+          <label>E-Mail</label>
+          <input type="email" id="email" placeholder="Mail Adresinizi Girin" />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default GeneralForm;
