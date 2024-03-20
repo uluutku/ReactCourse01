df6c6aa51a28df6a93404637d25ae18a22f946de
Utku Ulu <utku.ulu@bilgeadamboost.com>
2024-03-20 18:30:12 +0300

    updated burger site project

--- 12-BurgerSite/burger-site/src/Components/Menu.jsx
+++ 12-BurgerSite/burger-site/src/Components/Menu.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { Data } from "../helpers/Data";
+import "../styles/Menu.css";
+import MenuItem from "./MenuItem";
 
 export const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <div className="menu">
+      <h1 className="menuTitle">Burgerlerimiz</h1>
+      <div className="menuList">
+        {Data.map((menuItem, index) => {
+          return (
+            <MenuItem
+              image={menuItem.image}
+              name={menuItem.name}
+              content={menuItem.content}
+              price={menuItem.price}
+              key={index}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
